4f63d7886be9c0410fa6ebbc1c2ca5d38e94c1c6
Tijm van Dongen <i486333@Fhict.nl>
2024-12-16 18:21:03 +0100

    Added authMiddleware to add token to headers

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,11 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { ApolloClient, InMemoryCache, ApolloLink, concat } from '@apollo/client/core'
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 
 const cache = new InMemoryCache()
+const token = btoa("user:password")
 const uploadLink = createUploadLink({
   uri: 'http://localhost:8080/graphql', // Update with your server URL
 });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      Authorization: "basic "+token,
+    }
+  }));
+  return forward(operation);
+})
+
 export const apolloClient = new ApolloClient({
   cache,
-  link: uploadLink,
+  link: concat(authMiddleware,uploadLink),
 })
